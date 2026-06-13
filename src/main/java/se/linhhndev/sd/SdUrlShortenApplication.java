bf9883e91b9ad4a6866a5e34bb65e53a4f13ad84
LinhHN <LinhHNDev@gmail.com>
2026-06-13 18:06:32 +0700

    Initial commit

---
+++ src/main/java/se/linhhndev/sd/SdUrlShortenApplication.java
@@ -0,0 +1,13 @@
+package se.linhhndev.sd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdUrlShortenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdUrlShortenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/linhhndev/sd/SdUrlShortenApplicationTests.java
@@ -0,0 +1,13 @@
+package se.linhhndev.sd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdUrlShortenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
